ecc168123396fef3b7dca6a798b119898500ab44
PC <darlibelith@gmail.com>
2022-11-02 09:48:13 -0600

    Repositorio creado, pendiente vinculación con base de datos

--- app/src/main/java/com/example/agenda_citas_medicas/calificanos.java
+++ app/src/main/java/com/example/agenda_citas_medicas/calificanos.java
@@ -3,12 +3,28 @@ package com.example.agenda_citas_medicas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.Toast;
 
 public class calificanos extends AppCompatActivity {
+    RatingBar ratingBar;
+    Button boton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calificanos);
+        ratingBar = (RatingBar) findViewById(R.id.raiting);
+        boton = (Button) findViewById(R.id.raitingbutton);
+        boton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                String rating = String.valueOf(ratingBar.getRating());
+                Toast.makeText(getApplicationContext(), rating, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
